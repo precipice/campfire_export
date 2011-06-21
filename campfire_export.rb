b114ae9597a52d55056fa8e8b740b47e4d3e9722
Marc Hedlund <marc@precipice.org>
2011-06-21 15:17:38 -0400

    Add a license (Apache license).
    Since I didn't write all the code, the copyright only applies to my own commits, as reflected by the GitHub diffs.

--- campfire_export.rb
+++ campfire_export.rb
@@ -1,3 +1,28 @@
+#!/usr/bin/env ruby
+
+# Portions copyright 2011 Marc Hedlund <marc@precipice.org>.
+# Adapted from https://gist.github.com/821553 and ancestors.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# campfire_export.rb -- export Campfire transcripts and uploaded files.
+#
+# Since Campfire (www.campfirenow.com) doesn't provide an export feature,
+# this script implements one via the Campfire API.
+#
+# Configure the script below with your Campfire account details, or it will
+# not run.
+
 require 'rubygems'
 
 require 'cgi'
